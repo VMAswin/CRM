f91ba7217a45be3eddeccbe3b8d74a3d4d85f80a
VMAswin <aswinvm67@gmail.com>
2024-05-03 18:48:21 +0530

    sixth commit

--- CRMapp/urls.py
+++ CRMapp/urls.py
@@ -3,4 +3,6 @@ from .import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('dashboard',views.dashboard,name='dashboard'),
+    path('archives',views.archives,name='archives'),
+    path('test',views.test,name='test'),
 ]
--- CRMapp/views.py
+++ CRMapp/views.py
@@ -4,3 +4,7 @@ def home(request):
     return render(request,'landing_page.html')
 def dashboard(request):
     return render(request, 'dashboard.html')
+def archives(request):
+    return render(request,'archives.html')
+def test(request):
+    return render(request,'test.html')
